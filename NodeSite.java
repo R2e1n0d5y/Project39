a62391691ac8cc3d52584b2928bf623834bd517f
bang-dyy <ardindidy@gmail.com>
2024-12-15 21:32:43 +0800

    didyo

---
+++ NodeSite.java
@@ -0,0 +1,12 @@
+public class NodeSite {
+    String nama;
+    String pertanyaan;
+    String jawaban;
+
+    public NodeSite(String nama, String pertanyaan, String jawaban){
+        this.nama = nama;
+        this.pertanyaan = pertanyaan;
+        this.jawaban = jawaban;
+    }
+    
+}
---
+++ NodeWord.java
@@ -0,0 +1,10 @@
+public class NodeWord {
+    String nama;
+    String deskripsi;
+
+    public NodeWord(String nama, String deskripsi){
+        this.nama = nama;
+        this.deskripsi = deskripsi;
+    }
+    
+}
