6cbc0a19f396a8efa88546d9849aea5975f8430c
bang-dyy <ardindidy@gmail.com>
2024-12-15 21:11:24 +0800

    didyoo

---
+++ NodeBuku.java
@@ -0,0 +1,13 @@
+public class NodeBuku {
+    String nama;
+    String ability;
+    String wordl;
+    String site;
+
+    public NodeBuku(String nama, String ability, String wordl, String site){
+        this.nama = nama;
+        this.ability = ability;
+        this.wordl = wordl;
+        this.site = site;
+    }
+}
