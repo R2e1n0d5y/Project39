44b8a13b3d285a1fec2e1283f63080d959690982
Aditya <adityaadit092004@gmail.com>
2024-12-18 22:45:43 +0800

    Aditya

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
-public class MainAdit__i {
-        public static void textCenter(String text){
+public class Main {
+
+    public static void textCenter(String text){
         String spasiKiri = " ".repeat((75-text.length())/2);
         String spasiKanan = " ".repeat(75-((75-text.length())/2)-text.length());
         System.out.println("| |"+spasiKiri+text+spasiKanan+"| |");
@@ -29,7 +30,7 @@ public class MainAdit__i {
         queue.enqueue(world1);
         NodeKarakter karakter1 = new NodeKarakter(" ", 0, " ", " ", 0);
         world1.tree.root  = new NodeSite("Shadow Corridor", "Apa yang biasanya paling ditakuti oleh tokoh utama di dalam sebuah cerita?"+
-        "\na.Sosok Bayangan\nb.Kehilangan barang berharga\nc.Kehilangan arah\nd.Kegagalan dalam misi", "a", null, karakter1);
+                "\na.Sosok Bayangan\nb.Kehilangan barang berharga\nc.Kehilangan arah\nd.Kegagalan dalam misi", "a", null, karakter1);
 
         ll.add("Subaru", 17, "Male", "Fear", 0);
         ll.add("Reinhard", 22, "Male", "Fear", 0);
@@ -51,7 +52,7 @@ public class MainAdit__i {
             System.out.print("|=| ");
             int memilih  = inputUser.nextInt();
             if(memilih>0 && memilih <6){
-                karakter=ll.getKarakter(memilih); 
+                karakter=ll.getKarakter(memilih);
                 queue.first.tree.root.karakter=karakter;
                 break;
             }
@@ -60,6 +61,78 @@ public class MainAdit__i {
                 textLeft("Silahkan masukkan ulang dengan format 1/2/3/4/5");
             }
         }
-        inputUser.close();
+
+        while(true){//tempet finish blm dibuat
+            System.out.println(garis);
+            System.out.println(spasi);
+            textCenter(queue.first.nama);
+            System.out.println(spasi);
+            System.out.println(garis);
+            textCenter(karakter.name);
+            System.out.println(spasi);
+            textRight(karakter.name);
+            textRight(" "+karakter.age);
+            textRight(karakter.gender);
+            textRight("Jumlah Buku");
+            textRight("["+karakter.jumlahBuku+"]");
+            textRight("\\");
+
+            if(queue.first.tree.cekBuku()){
+                System.out.println(garis);
+                textLeft("Anda mendapatkan buku : "+queue.first.tree.findSiteKarakter().bukuThere.nama);
+                textLeft("at : "+queue.first.nama+"."+queue.first.tree.findSiteKarakter().name);
+                textLeft("ability : ........");
+                System.out.println(garis);
+                queue.first.tree.inBuku();
+            }
+
+            if(queue.first.tree.totalBuku()!=0){
+                while (true) {
+                    textLeft("Apakah anda ingin menggunakan buku? (y/t)");
+                    System.out.print("|=| ");
+                    char pilih  = inputUser.next().charAt(0);
+                    if(pilih=='y'){
+                        NodeBuku buku = queue.first.tree.outBuku();
+                        textLeft("buku yang digunakan : "+buku.nama);
+                        textLeft("didapatkan pada : "+buku.world+"."+buku.site);
+                        textLeft("dengan ability : "+buku.ability);
+                        System.out.println(garis);
+                        if(buku.ability=="Kembali ke site pertama dunia ini"){
+                            queue.first.tree.backToRoot();
+                            //tidak mejawab soal
+                        }
+                        else if(buku.ability=="Menuju ke ending"){
+                            System.out.println();
+                            System.out.println(garis);
+                            textCenter("Selamat Anda Berhasil Menuju Ending");
+                            textCenter("Dan Memenangkan Game Ini");
+                            System.out.println(garis);
+                            System.out.println("\nPress ENTER to continue...");
+                            inputUser.nextLine();
+                            return;
+                        }
+                        else if(buku.ability=="Menuju ke dunia berikutnya"){
+                            queue.pindahDunia();
+                            //tidak menjawab soal
+                        }
+                        else if(buku.ability=="Mendapatkan petunjuk jawaban yang benar"){
+                            queue.first.tree.cekJawaban();
+                        }
+                        else if(buku.ability=="Anda dapat mengetahui jalan berikutnya jadi site ini"){
+                            queue.first.tree.cekFuture();
+                        }
+                        break;
+                    }
+                    else if(pilih=='t'){
+                        break;
+                    }
+                    else{
+                        textLeft("format jabawan yang anda masukkan salah");
+                        textLeft("Silahkan masukkan ulang dengan format y/t");
+                    }
+                }
+            }
+            inputUser.close();
+        }
     }
 }
